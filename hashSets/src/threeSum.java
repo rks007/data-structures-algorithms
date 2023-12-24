5b0496a6ef26f53b13903d950516bfb7b6186ab5
Rishabh Kumar Singh <rishabhkumarsingh94@gmail.com>
2023-12-24 22:18:56 +0530

    three sum problem

---
+++ hashSets/src/threeSum.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+public class threeSum { //leetcode 15
+
+    public static List<List<Integer>> threeSum(int[] nums){ //brute force  t.c = n^3
+        HashSet<List<Integer>> st = new HashSet<>();
+        for (int i = 0;i < nums.length;i++){
+            for (int j = i + 1;j < nums.length;j++){
+                for (int k = j + 1;k < nums.length;k++){
+                    if (nums[i] + nums[j] + nums[k] == 0){
+                        List<Integer> temp = Arrays.asList(nums[i],nums[j],nums[k]);
+                        temp.sort(null); //sorting this to be sure that if any time an list occur same as previous any list then it will not add in hashset
+                        st.add(temp);
+                    }
+                }
+            }
+        }
+        List<List<Integer>> ans = new ArrayList<>(st);
+        return ans;
+
+    }
+
+    public static List<List<Integer>> threeSum(int[] nums) {//better aproach  t.c = n^2
+        HashSet<List<Integer>> st = new HashSet<>();
+
+        for (int i = 0;i < nums.length;i++){
+            HashSet<Integer> hashset = new HashSet<>(); //it will reset after every change in i
+            for (int j = i + 1;j < nums.length;j++){
+                int third = -(nums[i] + nums[j]);
+                if (hashset.contains(third)){
+                    List<Integer> temp = Arrays.asList(nums[i], nums[j], third);
+                    temp.sort(null);
+                    st.add(temp);
+                }
+                else{
+                    hashset.add(nums[j]); //adding this because if not found third then j will move to next index so before moving add j indexed value to the hashset
+                }
+            }
+        }
+        List<List<Integer>> ans = new ArrayList<>(st);
+        return ans;
+    }
+}
