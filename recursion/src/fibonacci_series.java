7f34cd7c81a4a6cce9f6beba5f80b571d1724343
Rishabh Kumar Singh <rishabhkumarsingh94@gmail.com>
2024-01-06 16:19:34 +0530

    not imp changes

--- recursion/src/fibonacci_series.java
+++ recursion/src/fibonacci_series.java
@@ -1,11 +1,11 @@
 public class fibonacci_series {
     public static void main(String[] args) {
-        System.out.println(fibo(5 ));
+        System.out.println(fibo(7 ));
 
     }
     public static int fibo(int n){
-        if (n < 2){
-            return n;
+        if (n == 1 || n == 2){
+            return 1;
         }
 
         return fibo(n-1)+fibo(n-2);
