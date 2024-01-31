c4bf220f90c279524ab8c4292c0593ae2e1a90b5
Rishabh Kumar Singh <rishabhkumarsingh94@gmail.com>
2024-01-31 16:38:56 +0530

    code modified

--- Arrays/arrays/src/nonDecreasing.java
+++ Arrays/arrays/src/nonDecreasing.java
@@ -1,8 +1,8 @@
 package arrays.src;
 
-public class nonDecreasing { //https://www.codingninjas.com/studio/problems/non-decreasing-array_699920?utm_source=youtube&utm_medium=affiliate&utm_campaign=parikh_youtube&count=25&page=1&search=&sort_entity=order&sort_order=ASC&leftPanelTabValue=PROBLEM
+public class nonDecreasing { //leetcode 665
     public static void main(String[] args) {
-        int[] arr = {-71, -46, -41, -8, 7, 35, 219, -150, 57, 59};
+        int[] arr = {-1,4,2,3};
         int n = arr.length;
 
         boolean ans = isPossible(arr,n);
@@ -10,19 +10,23 @@ public class nonDecreasing { //https://www.codingninjas.com/studio/problems/non-
     }
     public static boolean isPossible(int[] arr, int n) {
         int count = 0;
-        for (int i = 0;i < n - 1;i++){
-            if (arr[i] > arr[i + 1]){
+        for (int i = 0;i < n - 1;i++) {
+            if (arr[i] > arr[i + 1]) {
                 count++;
+
+                if(count > 1){
+                    return false;
+                }
+                //this is for checking that which number needs modification
+                // i > 0 so that array out of bound does not occur
+                if(i > 0 && arr[i + 1] < arr[i - 1]){
+                    arr[i + 1] = arr[i];
+                }
+                else {
+                    arr[i] = arr[i + 1];
+                }
             }
-            if (i > 0 && arr[i + 1] < arr[i - 1]) { //very important step where if convert the smallest element of i + 1 to the same value of i so that the further calculations will be correct {-71, -46, -41, -8, 7, 35, 219, -150, 57, 59}
-                arr[i + 1] = arr[i]; // Modify the next element
-            }
         }
-        if (count > 1){
-            return false;
-        }
-        else {
-            return true;
-        }
+        return true;
     }
 }
