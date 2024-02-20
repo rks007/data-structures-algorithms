02a3b687553b89cd5e65b007afd31f0a888a6a6a
Rishabh Kumar Singh <rishabhkumarsingh94@gmail.com>
2024-02-20 09:58:51 +0530

    minor changes

--- basicMaths/src/oddEven.java
+++ basicMaths/src/oddEven.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class oddEven {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter the number");
@@ -9,6 +9,7 @@ public class Main {
     }
 
     private static boolean isOdd(int n) {
+        //we know that any number and with 1 gives the same number back,and we also know this thing with the observation that odd number in binary form has last digit as 1
         return (n & 1) == 1;
     }
 }
