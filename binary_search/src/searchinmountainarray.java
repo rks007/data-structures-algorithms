49649f5fee32a0949191b560eecf2e526374f619
Rishabh Kumar Singh <rishabhkumarsingh94@gmail.com>
2024-05-03 00:15:46 +0530

    add small changes

--- binary_search/src/searchinmountainarray.java
+++ binary_search/src/searchinmountainarray.java
@@ -7,16 +7,16 @@ public class searchinmountainarray {
         int start = 0;
         int end = arr.length-1;
         while (start < end){
-            //in binary search we gnerally give <= condition but here we give only < because of some reasons and i think i already know the raesons
+            //in binary search we gnerally give <= condition but here we give only < because of some reasons and i think i already know the raesons or you have any confusion just dry run the solution
             int mid = start + (end - start) / 2;
-            if (arr[mid] > arr[mid + 1]){
+            if (arr[mid] > arr[mid + 1]){//dry run
                 end = mid;
             }
-            else {
+            else { //dry run
                 start = mid + 1;
             }
         }
-        return start;
+        return start;//if you dont know the reson to return the start then please dry run the whole solution to understand the logic behind
     }
 }
 
