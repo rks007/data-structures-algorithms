509c31c1d0ac9f2ce82ca11c3f2022427314b368
Rishabh Kumar Singh <rishabhkumarsingh94@gmail.com>
2024-05-05 23:18:49 +0530

    add trapping rain water approach 1

--- Arrays/arrays/src/trappingRainWater1.java
+++ Arrays/arrays/src/trappingRainWater1.java
@@ -16,17 +16,17 @@ public class trappingRainWater1 {//leetcode 42
         int[] rightMaxArr = new int[height.length];//we make an array where we have store the maximum height from right to left for each index
         int maxRight = -1;
 
-        for(int i = 0; i < height.length - 1;i++){//process of storing
+        for(int i = 0; i < height.length;i++){//process of storing, prefix
             maxLeft = Math.max(maxLeft, height[i]);
             leftMaxArr[i] = maxLeft;
         }
 
-        for(int i = height.length - 1; i >= 0;i--){//process of storing
+        for(int i = height.length - 1; i >= 0;i--){//process of storing, suffix
             maxRight = Math.max(maxRight, height[i]);
             rightMaxArr[i] = maxRight;
         }
         int ans = 0;
-        for (int i = 0; i < height.length - 1; i++){//now just calculate each block capacity to store the rain water
+        for (int i = 0; i < height.length ; i++){//now just calculate each block capacity to store the rain water
 
             ans += Math.min(leftMaxArr[i], rightMaxArr[i]) - height[i];//main crux, please dry run
         }
