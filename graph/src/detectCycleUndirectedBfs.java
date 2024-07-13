2fc5203fe5ff9d9f6334d8bbdf67f4c6cbba0e1c
Rishabh Kumar Singh <rishabhkumarsingh94@gmail.com>
2024-07-14 00:10:45 +0530

    add comments

--- graph/src/detectCycleUndirectedBfs.java
+++ graph/src/detectCycleUndirectedBfs.java
@@ -2,7 +2,31 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 /*
-explanation
+------------------------------------Explanation-----------------------------
+
+Cycle Detection in Undirected Graph using BFS
+Class: Solution
+
+Nested Class: Pair
+Holds a node (first) and its parent (second).
+Method: isCycle
+
+Parameters: int V (number of vertices), ArrayList<ArrayList<Integer>> adj (adjacency list).
+Initializes a vis array to track visited nodes.
+Uses a BFS approach with a queue q.
+Iterates through all nodes. If a node hasn't been visited, calls detectCycle to check for cycles from that node.
+Method: detectCycle
+
+Parameters: int node, boolean[] vis, Queue<Pair> q, ArrayList<ArrayList<Integer>> adj.
+Marks the starting node as visited and adds it to the queue with -1 as its parent.
+While the queue is not empty:
+Processes each node's adjacent nodes:
+If an adjacent node is not visited, marks it as visited and adds it to the queue with the current node as its parent.
+If an adjacent node is visited and is not the parent of the current node, a cycle is detected.
+Returns true if a cycle is found, otherwise false.
+Key Point:
+
+Correctly updates the parent node when adding nodes to the queue to ensure accurate cycle detection.
 
 */
 
@@ -23,7 +47,7 @@ public class detectCycleUndirectedBfs {
         boolean[] vis = new boolean[V];
         Queue<Pair> q = new LinkedList<>();
 
-        for(int i = 0; i < V; i++){
+        for(int i = 0; i < V; i++){// doing this because of disconnected graph can also be there in the input
             if(vis[i] == false){
                 if(detectCycle(i, vis, q, adj)){
                     return true;
